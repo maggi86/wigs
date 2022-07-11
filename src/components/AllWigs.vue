14133215575e6303ed002a8042b6c747781d1eb2
GarlicBuns19 <planterx26@gmail.com>
2022-07-11 15:50:27 +0200

    The wigs are displaying

---
+++ src/components/AllWigs.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul>
+        <div v-if="wigs">
+        <li v-for="wig in wigs" :key="wig">
+            <p>{{wig.name}} R{{wig.price}}</p>
+        </li>
+        </div>
+        <div v-else>
+            Loading...
+        </div>
+    </ul>
+</template>
+<script>
+
+export default {
+    mounted(){
+        this.$store.dispatch('fetchAllWigs')
+    },
+    computed: {
+        wigs(){
+            return this.$store.state.wigs
+        }
+    }
+}
+</script>
---
+++ src/views/WigsForSale.vue
@@ -0,0 +1,12 @@
+<template>
+    <AllWigs/>
+</template>
+<script>
+import AllWigs from '../components/AllWigs.vue'
+
+export default {
+    components: {
+        AllWigs
+    }
+}
+</script>
