db6884976a910d3cce51568b98e4ea6460b87195
GarlicBuns19 <planterx26@gmail.com>
2022-07-11 15:17:59 +0200

    Added db of wigs

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: null,
+    wigs: null
   },
   getters: {
+    wigs(state){
+      return state.wigs
+    }
   },
   mutations: {
+    setWigs(state, wigs){
+      state.wigs = wigs
+    }
   },
   actions: {
+    fetchAllWigs(context){
+      fetch('http://localhost:3000/wigs')
+      .then(res => res.json())
+      .then(data => context.state.wigs = data)
+    }
   },
   modules: {
   }
