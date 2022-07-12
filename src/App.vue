0ba332b8a2728badf0b744f00986ccbc536d195d
TOP-FLOOR-BOSS <engelliam71@gmail.com>
2022-07-12 16:17:11 +0200

    Needs css

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
-
 <template>
-
-<div>
-<Navbar />
-</div>
-
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
   <router-view/>
-  
 </template>
 
-<script>
-// @ is an alias to /src
-import Navbar from './components/Navbar.vue'
-// import Foot from '@/components/Foot.vue'
-
-export default {
-  components: {
-    Navbar
-    // Foot
-},
-
-};
-
-
-
-
-</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -33,8 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
 
-}</style>
+nav {
+  padding: 30px;
+}
 
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
