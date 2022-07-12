a65322b7783f47a7be8f09f9d7dc79679e5ace76
GarlicBuns19 <planterx26@gmail.com>
2022-07-12 20:31:34 +0100

    Need to fix delete and edit

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,55 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
     user: null,
     wigs: null,
-    wig: {}
+    wig: {},
   },
   getters: {
-    wigs(state){
-      return state.wigs
-    }
+    wigs(state) {
+      return state.wigs;
+    },
   },
   mutations: {
-    setWigs(state, wigs){
-      state.wigs = wigs
+    setWigs(state, wigs) {
+      state.wigs = wigs;
+    },
+    deleteWig(id) {
+      fetch(`http://localhost:3000/wigs/`+ id , {
+        method: "DELETE",
+      })
+        // .commit('removeWig',id)
+    },
+    addWig(context) {
+      fetch("http://localhost:3000/wigs/" ,{
+        method: "POST",
+        body: JSON.stringify({
+          name: "foo",
+          rating: "1 star",
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => (context.state.wigs = data));
     }
+    // removeWig(state,id){
+    //   state.wigs = state.wigs.filter(wig => wig.id !== id)
+    // }
   },
   actions: {
-    fetchAllWigs(context){
-      fetch('http://localhost:3000/wigs')
-      .then(res => res.json())
-      .then(data => context.state.wigs = data)
+    fetchAllWigs(context) {
+      fetch("http://localhost:3000/wigs")
+        .then((res) => res.json())
+        .then((data) => (context.state.wigs = data));
     },
-    fetchSingleWig(context, id){
-      fetch('http://localhost:3000/wigs/' + id)
-      .then(res => res.json())
-      .then(data => context.state.wig = data)
-    }
+    fetchSingleWig(context, id) {
+      fetch("http://localhost:3000/wigs/" + id)
+        .then((res) => res.json())
+        .then((data) => (context.state.wig = data));
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
